f2302ff86a36be11760a154e95051f83c1d5a8b5
David W. Gerbing <davidg@sba.pdx.edu>
2010-11-03 07:41:10 +0000

    version 1.5.2

--- R/rad.R
+++ R/rad.R
@@ -21,37 +21,48 @@ function (...) {
 pre <- ">"
 line <- "------------------------------------------------------------\n"
 
+cat("\n")
 if (is.null(ref)) {
-	cat(line, pre, " mydata <- read.csv(file.choose())", "\n", line, sep="")
+	cat(line, pre, " mydata <- read.csv(file.choose())", "\n", sep="")
 	ref <- file.choose()
 	cat("\nFile: ", ref, "\n")
 }
 else {
-	cat(line, pre, " mydata <- read.csv(file=\"",ref,"\")", "\n", line, sep="")
+	cat(line, pre, " mydata <- read.csv(file=\"",ref,"\")", "\n", sep="")
 }
 mydata <<- read.csv(file=ref, ...)
+
+cat(pre, " attach(mydata)", "\n", line, sep="")
+attach(mydata, warn.conflicts=FALSE)
 
 if (nargs() > 1) cat("Plus the optional arguments that were entered in rad.", "\n")
 cat("\n")
 cat("Name of data frame that contains the data:   mydata ", "\n")
 cat("Number of Variables in mydata:    ", ncol(mydata), "\n")
-cat("Number of Rows of Data in mydata: ", nrow(mydata), "\n\n")
+cat("Number of Rows of Data in mydata: ", nrow(mydata), "\n")
 
-cat(line, pre, " attach(mydata)", sep="", "\n")
-attach(mydata, warn.conflicts=FALSE)
-
-cat(pre, " head(mydata, n=3)", "\n", line, sep="", "\n")
+cat("\n\n")
+cat(line, pre, " head(mydata, n=3)   # First three rows", "\n", line, sep="", "\n")
 send.to.console(head(mydata, n=3))
 
-cat("\n")
-cat(line, pre, " tail(mydata, n=3)", "\n", sep="", line, "\n")
+cat("\n\n")
+cat(line, pre, " tail(mydata, n=3)   # Last three rows", "\n", sep="", line, "\n")
 send.to.console(tail(mydata, n=3))
 
-cat("\n")
-cat(line, "What is next? Can enter one of the following.", "\n", sep="", line)
-cat("mydata: List all rows of data.\n")
-cat("str(mydata): Detailed look at each variable in mydata.\n")
-cat("help.me(): List of topics for analysis with related R functions.")
+cat("\n\n")
+cat(line, pre, " str(mydata, digits.d=15)   # Types of variables", "\n", sep="")
+cat(line)
+cat("Factor: Variable with non-numeric categories or levels, stored as an integer.\n")
+cat("int: Numeric variable limited to integer values.\n")
+cat("num: Numeric variable that may have decimal digits.\n")
+cat(line, "\n")
+send.to.console(str(mydata, digits.d=15))
+
+cat("\n\n")
+cat(line, "What is next? Try one of the following.", "\n", sep="", line)
+cat("mydata: List all rows (observations) of data.\n")
+cat("help.me(): List of topics for analysis with related R/lessR functions.")
 cat("\n", line, sep="")
+cat("\n")
 
 }
